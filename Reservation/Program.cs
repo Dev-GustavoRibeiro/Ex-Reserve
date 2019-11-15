b7f398d6b8e236796adea82dcd7530f6cc33b09a
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2019-11-14 22:46:42 -0300

    Finished

---
+++ Reservation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ReservationSystem.Entities;
+using ReservationSystem.Entities.Exceptions;
+
+namespace ReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime chekIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+                Reservation reservation = new Reservation(roomNumber, chekIn, checkOut);
+
+                Console.WriteLine(reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                chekIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(chekIn, checkOut);
+                Console.WriteLine(reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+        }
+    }
+}
